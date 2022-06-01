cef52678a23c81d01df37b6b472d8b682e486730
Shivam <97354675+mr-shivamgarg@users.noreply.github.com>
2022-06-01 12:53:34 +0000

    Update Pieces.cpp

--- Pieces.cpp
+++ Pieces.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int A[8][8] = {0};
@@ -8,8 +9,9 @@ struct coordinates
     int y; // column
 };
 
-enum piecenames{
-    pawnb,
+enum piecenames
+{
+    pawnb = 1,
     rookb,
     knightb,
     bishopb,
@@ -33,58 +35,75 @@ public:
     coordinates getposition();
     bool getcolor();
     void setcolor(bool);
+    // void possiblemove();
 };
 
 class pawn : public pieces
 {
     bool firstMove;
+
 public:
     pawn(bool, int);
     void setposition(coordinates);
+    //  void possiblemove();
     // pawn();
 };
 
-class queen: public pieces
+class queen : public pieces
 {
+public:
     queen(bool);
+    void setposition(coordinates a);
 };
 
 class king : public pieces
 {
+public:
     king(bool);
+    void setposition(coordinates a);
 };
 class knight : public pieces
 {
-    bool left;  //0 for right and 1 for left
-    knight(bool);
+    bool isleft; // 0 for right and 1 for left
+public:
+    knight(bool,bool);
+    void setposition(coordinates a);
 };
 class rook : public pieces
 {
-    bool left;  
-    rook(bool);
+    bool isleft;
+public:
+    rook(bool,bool);
+    void setposition(coordinates a);
 };
 class bishop : public pieces
 {
-    bool left;  
-    bishop(bool);
-};
+    bool isleft;
 
+public:
+    bishop(bool,bool);
+    void setposition(coordinates a);
+};
 
 int main(int argc, char **argu)
 {
     cout << "Hello World\n";
     pawn wpawn[] = {pawn(1, 0), pawn(1, 1), pawn(1, 2), pawn(1, 3), pawn(1, 4), pawn(1, 5), pawn(1, 6), pawn(1, 7)};
     pawn bpawn[] = {pawn(0, 0), pawn(0, 1), pawn(0, 2), pawn(0, 3), pawn(0, 4), pawn(0, 5), pawn(0, 6), pawn(0, 7)};
+    queen bqueen(0), wqueen(1);
+    king bking(0), wking(1);
+    knight blknight(0,1),brknight(0,0), wlknight(1,1),wrknight(1,0);
+    bishop blbishop(0,1),brbishop(0,0), wlbishop(1,1),wrbishop(1,0);
+    rook blrook(0,1),brrook(0,0), wlrook(1,1),wrrook(1,0);
 
     // for(int i=0;i<8;i++)
     // {
     //     wpawn[i].setposition(i);
     // }
-
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
-            cout << A[i][j];
+            cout << setw(3) << A[i][j];
         cout << endl;
     }
     return 0;
@@ -108,11 +127,11 @@ void pieces ::setposition(coordinates a)
 
 void pawn ::setposition(coordinates a)
 {
-    setposition(a);
+    pieces::setposition(a);
     if (getcolor())
-        A[a.x][a.y]=pawnw;
+        A[a.x][a.y] = pawnw;
     else
-        A[a.x][a.y]=pawnb;
+        A[a.x][a.y] = pawnb;
 }
 coordinates pieces::getposition()
 {
@@ -121,12 +140,22 @@ coordinates pieces::getposition()
 pawn ::pawn(bool c, int y)
 {
     setcolor(c);
-    firstMove=true;
+    firstMove = true;
     if (c)
         setposition({1, y});
     else
         setposition({6, y});
 }
+// bool pawn::possiblemove()
+// {
+//     if(firstMove)
+//     {
+//         if(A[a.x+2][y])
+//         {
+//             //Highlight
+//         }
+//     }
+// }
 queen ::queen(bool c)
 {
     setcolor(c);
@@ -134,6 +163,14 @@ queen ::queen(bool c)
         setposition({0, 4});
     else
         setposition({7, 4});
+}
+void queen::setposition(coordinates a)
+{
+    pieces::setposition(a);
+    if (getcolor())
+        A[a.x][a.y] = queenw;
+    else
+        A[a.x][a.y] = queenb;
 }
 king ::king(bool c)
 {
@@ -143,9 +180,10 @@ king ::king(bool c)
     else
         setposition({7, 3});
 }
-knight ::knight(bool c)
+knight ::knight(bool c, bool d)
 {
     setcolor(c);
+    isleft=d;
     if (c && left)
         setposition({0, 1});
     else if (c)
@@ -155,9 +193,10 @@ knight ::knight(bool c)
     else
         setposition({7, 6});
 }
-bishop ::bishop(bool c)
+bishop ::bishop(bool c, bool d)
 {
     setcolor(c);
+    isleft=d;
     if (c && left)
         setposition({0, 2});
     else if (c)
@@ -167,9 +206,10 @@ bishop ::bishop(bool c)
     else
         setposition({7, 5});
 }
-rook ::rook(bool c)
+rook ::rook(bool c, bool d)
 {
     setcolor(c);
+    isleft=d;
     if (c && left)
         setposition({0, 0});
     else if (c)
@@ -178,4 +218,36 @@ rook ::rook(bool c)
         setposition({7, 0});
     else
         setposition({7, 7});
+}
+void rook::setposition(coordinates a)
+{
+    pieces::setposition(a);
+    if (getcolor())
+        A[a.x][a.y] = rookw;
+    else
+        A[a.x][a.y] = rookb;
+}
+void knight::setposition(coordinates a)
+{
+    pieces::setposition(a);
+    if (getcolor())
+        A[a.x][a.y] = knightw;
+    else
+        A[a.x][a.y] = knightb;
+}
+void bishop::setposition(coordinates a)
+{
+    pieces::setposition(a);
+    if (getcolor())
+        A[a.x][a.y] = bishopw;
+    else
+        A[a.x][a.y] = bishopb;
+}
+void king::setposition(coordinates a)
+{
+    pieces::setposition(a);
+    if (getcolor())
+        A[a.x][a.y] = kingw;
+    else
+        A[a.x][a.y] = kingb;
 }
