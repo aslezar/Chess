2b9b94166fa0c6f4170abf2c7ae5b182c3cf8d4a
Shivam <97354675+mr-shivamgarg@users.noreply.github.com>
2022-06-03 09:02:10 -0700

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,4 @@
-#include "Pieces.h"
+#include "pieces.cpp"
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -9,17 +9,31 @@ void DisplayBoard()
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
-            cout << setw(3) << A[i][j];
+        {
+            if (board[i][j] != 0)
+            {
+                cout << setw(3) << board[i][j]->getname() << ((board[i][j]->getcolor())?'w':'b');
+            }
+            else
+            {
+                cout<<setw(4)<<0;
+            }
+            
+        }
         cout << endl;
     }
 }
 
-bool move(int a[4]) {
+bool move(int a[4])
+{
+    if (board[a[0]][a[1]]!=0)
+    {
+        board[a[0]][a[1]]->move({a[2],a[3]});
+    }
     return 0;
-
 }
 
-int main(int argc, char argu)
+int main()
 {
     cout << "Hello World\n";
     pawn wpawn[] = {pawn(1, 0), pawn(1, 1), pawn(1, 2), pawn(1, 3), pawn(1, 4), pawn(1, 5), pawn(1, 6), pawn(1, 7)};
@@ -29,14 +43,6 @@ int main(int argc, char argu)
     knight blknight(0, 1), brknight(0, 0), wlknight(1, 1), wrknight(1, 0);
     bishop blbishop(0, 1), brbishop(0, 0), wlbishop(1, 1), wrbishop(1, 0);
     rook blrook(0, 1), brrook(0, 0), wlrook(1, 1), wrrook(1, 0);
-    // pieces *piecesw[16];
-    // pieces[0]=&wqueen;
-    // pieces[0]->setpositon(1,1);
-
-    // for(int i=0;i<8;i++)
-    // {
-    //     wpawn[i].setposition(i);
-    // }
     DisplayBoard();
     bool n = 0;
     int a[4];
@@ -44,12 +50,15 @@ int main(int argc, char argu)
     {
         cout << "Enter the position from which you want to move the piece : ";
         cin >> a[0] >> a[1];
+        cin.clear();
         cout << "Enter the position where you want to move the piece : ";
         cin >> a[2] >> a[3];
+        cin.clear();
         move(a);
         DisplayBoard();
         cout << "Do you want to make another move(0/1) : ";
         cin >> n;
+        cin.clear();
 
     } while (n);
 
